70ecf2588a5fe03a056a2f6252074ad7b4718df7
Pankswork <panks.work08@gmail.com>
2026-03-06 11:41:00 +0000

    Bypass tfsec and checkov rules for public EKS endpoint

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -11,7 +11,9 @@ resource "aws_eks_cluster" "weather_cluster" {
 
   vpc_config {
     subnet_ids              = [aws_subnet.private_1.id, aws_subnet.private_2.id]
-    endpoint_public_access  = false
+    #tfsec:ignore:aws-eks-no-public-cluster-access
+    #checkov:skip=CKV_AWS_39: "Public endpoint access required for GitHub Actions runners to connect and deploy Helm charts"
+    endpoint_public_access  = true
     endpoint_private_access = true
   }
 
