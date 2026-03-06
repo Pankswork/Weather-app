14fa52d12a0c0f6eace085f011efe1f7e8334e1b
Pankswork <panks.work08@gmail.com>
2026-03-06 08:45:28 +0000

    Implemented devsecops

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.5.7"
 
   backend "s3" {
-    bucket         = "weather-app-panks-bucket"
+    bucket         = "weather-app-panks-bucketss"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
